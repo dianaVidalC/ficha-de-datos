816c18b2ec9ba08fc647771b23033fb5c87d37d1
Erika Vidal <erikadianavidalcapillo@gmail.com>
2017-03-24 18:49:46 -0500

    corrigiendo validación

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,12 +1,31 @@
-var nombre = document.getElementById("nombre");
-var apellido = document.getElementById("apellido");
-var dni = document.getElementById("dni");
-var direccion = document.getElementById("direccion");
+window.addEventListener("load",function(){
+  var nombre = document.getElementById("nombre");
+  var apellido = document.getElementById("apellido");
+  var dni = document.getElementById("dni");
+  var direccion = document.getElementById("direccion");
 
-function enviaDatos(){
-  document.getElementById("datos").innerHTML= "<ul><li>Nombre:" + nombre.value +
-                                            "<li>Apellido: " + apellido.value
-                                            + "<li>DNI: "+ dni.value+ "<li>Dirección: "
-                                            + direccion.value + "</ul>";
+  var boton=document.getElementById("boton");
 
-};
+  boton.addEventListener("click",function(e){
+    e.preventDefault();
+
+  if((!(/^[a-z\d_]{4,15}$/i.test(dni.value)))){
+    alert("Ingrese un nombre válido");
+    return false;
+  }else if (!(/^[a-z\d_]{4,15}$/i.test(dni.value))){
+    alert("Ingrese un apellido válido");
+    return false;
+  }else if (!(/^\d{8}$/.test(dni.value))){
+    alert("Ingrese un número de DNI con 8 dígitos");
+    return false;
+  }else if (!(/^[0-9a-zA-Z]+$/.test(dni.value))){
+    alert("Ingrese una direccion válida");
+    return false;
+  }else{
+    document.getElementById("datos").innerHTML= "<ul><li>Nombre:" + nombre.value +
+                                              "<li>Apellido: " + apellido.value
+                                              + "<li>DNI: "+ dni.value+ "<li>Dirección: "
+                                              + direccion.value + "</ul>";
+                                            }
+  });
+});
